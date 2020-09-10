237492e8fa73aa7db09eda2ccdad446bedfe1bb1
Jared Rudnicki <49525807+jaredrudnicki@users.noreply.github.com>
2020-09-09 22:03:48 -0400

    Update pyauto_test.py

--- pyauto_test.py
+++ pyauto_test.py
@@ -19,11 +19,9 @@ pa.press('delete')
 pa.write('cmd')
 pa.press('enter')
 
-#client_id = pa.prompt(text='enter client_id', title='client_id', default='')
-#client_secret = pa.prompt(text='enter client_secret', title='client_secret', default='')
-#redirect_uri = pa.prompt(text='enter redirect_uri', title='redirect_uri', default='http://google.com/')
 pa.write('cd OneDrive\n')
 pa.write('cd Desktop\n')
+pa.write('cd Spotipy-Graphing-master\n')
 
 pa.write('SET client_secret = \'6a112ea19cc04a64b18164c1218198cb\'\n')
 pa.write('SET client_id = \'ac16b07b2e97494eab56f93868924969\'\n')
